76205d31fe66ad960dc73395ae3765535eb60818
GouthamBabu <43052583+GouthamBabu@users.noreply.github.com>
2021-06-21 15:45:58 -0400

    Add files via upload

---
+++ Directions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DirectionsApp.Models
+{
+    public class Directions
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string TotalDuration { get; set; }
+        public string TotalDistance { get; set; }
+        public List<string> Route { get; set; }
+        public string MapURL { get; set; }
+    }
+}
---
+++ Parameters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectionsApp.Models
+{
+    public class Parameters
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+        public string FileName { get; set; }
+        public bool map { get; set; }
+    }
+}
