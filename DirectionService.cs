cf1ab0dd5c85e2013bb3780562a4e95572f15293
GouthamBabu <43052583+GouthamBabu@users.noreply.github.com>
2021-06-21 15:48:34 -0400

    Add files via upload

---
+++ DirectionService.cs
@@ -0,0 +1,59 @@
+﻿using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Maps.Directions.Request;
+using GoogleApi.Entities.Maps.Directions.Response;
+using GoogleApi.Entities.Maps.StaticMaps.Request;
+using System.Threading.Tasks;
+using System.Configuration;
+using DirectionsApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using System.Drawing.Imaging;
+
+namespace DirectionsApp
+{
+    public static class DirectionService
+    {
+        public static async Task<Directions> GetDirectionsAsync(Parameters UserParameters)
+        {
+            var request = new DirectionsRequest();
+            request.Key = ConfigurationManager.AppSettings.Get("apikey");
+            request.Origin = new Location(UserParameters.Start);
+            request.Destination = new Location(UserParameters.End);
+            //request.DepartureTime = UserParameters.DepartureTime;
+
+            var googleResponse = await GoogleApi.GoogleMaps.Directions.QueryAsync(request);
+
+            var DirectionsResponse = new Directions();
+            DirectionsResponse.Start = UserParameters.Start;
+            DirectionsResponse.End = UserParameters.End;
+            foreach (var Leg in googleResponse.Routes.First().Legs)
+            {
+                DirectionsResponse.TotalDistance += Leg.Distance.Value;
+                DirectionsResponse.TotalDuration += Leg.Duration.Value;
+                DirectionsResponse.Route = new List<string>();
+                if (UserParameters.map)
+                {
+                    var mapPoints = new List<Location>();
+                    var mapPaths = new List<MapPath>();
+                    foreach (var step in Leg.Steps)
+                    {
+                        mapPoints.Add(step.StartLocation);
+                    }
+                    mapPaths.Add(new MapPath() { Points = mapPoints });
+                    var staticMapRequest = new StaticMapsRequest()
+                    {
+                        Paths = mapPaths,
+                        Key = request.Key
+                    };
+                    DirectionsResponse.MapURL = staticMapRequest.GetUri().ToString();
+                }
+                foreach (var step in Leg.Steps)
+                {
+                    DirectionsResponse.Route.Add($"\n{step.HtmlInstructions.ToString()}");
+                }
+            }
+            return DirectionsResponse;
+        }
+    }
+}
---
+++ ParametersService.cs
@@ -0,0 +1,80 @@
+﻿using DirectionsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DirectionsApp
+{
+    public static class ParametersService
+    {
+        public static Parameters GetParameters()
+        {
+                var UserParameters = new Parameters();
+
+                UserParameters.Start = GetInput("Enter Starting Location: ");
+                UserParameters.End = GetInput("Enter Destination: ");
+                UserParameters.FileName = GetFileName();
+                UserParameters.map = Getmap();
+                return UserParameters;
+        }
+
+        private static string GetInput(string message)
+        {
+            Console.Write(message);
+            string value = "";
+            do
+            {
+                value = Console.ReadLine();
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Input required. " + message);
+                }
+
+            } while (string.IsNullOrEmpty(value));
+            return value;
+        }
+
+        private static string GetFileName()
+        {
+            bool valid = false;
+            do
+            {
+                Console.Write("Do you want to output to a file? (Yes or No) ");
+                string outputfile = Console.ReadLine();
+                if (outputfile.ToLower() == "yes")
+                {
+                    valid = true;
+                    Console.Write("Name your file: ");
+                    string fileName = Console.ReadLine() + ".text";
+                    return fileName;
+                }
+                else if (outputfile.ToLower() == "no")
+                {
+                    valid = true;
+                }
+                else
+                {
+                    Console.Write("Invalid, please enter Yes or No, ");
+                }
+            } while (!valid);
+            return null;
+        }
+
+
+        private static bool Getmap()
+        {
+           Console.Write("Would you like to add a map?(Yes or No)");
+            bool map = false;
+            var inputString = Console.ReadLine();
+            if (inputString.ToLower() == "yes")
+            {
+                map = true;
+            }
+            if (inputString.ToLower() == "no")
+            {
+                map = false;
+            }
+            return map;
+        }
+    }       
+}
